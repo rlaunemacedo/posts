62304ed387815b35362263407976a93b52b84de4
rlaunemacedo <rlaunemacedo@gmail.com>
2023-12-03 17:16:06 -0300

    Atualização 2023-12-03 17:16:06

--- atualiza.sh
+++ atualiza.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Atualizando o site rlaunemacedo.github.io.git"
+echo "Atualizando o site rlaunemacedo.github.io/posts"
 git add .
 git commit -m "Atualização $(date +%F\ %T)"
 git push -f
